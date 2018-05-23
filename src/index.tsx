53bc35ca15fb7ec5537043b91e5ab3a88bb28b59
Nico Bellante <nicobell@microsoft.com>
2018-05-23 13:11:03 -0700

    added hot reloading

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,14 @@ import 'normalize.css'
 import './index.css'
 
 import registerServiceWorker from './registerServiceWorker'
+const rootEl = document.getElementById('root') as HTMLElement
+ReactDOM.render(<App />, rootEl)
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
 registerServiceWorker()
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(<NextApp />, rootEl)
+  })
+}
