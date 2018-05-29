209d01af933ea09e8b9e27d8f210b82822fb478c
Nico Bellante <nicobell@microsoft.com>
2018-05-29 12:08:34 -0700

    editor resizing logic + command bar in editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -4,11 +4,28 @@ import styled from 'styled-components'
 import MonacoEditor from 'react-monaco-editor'
 import { ISnippet, ISnippetField } from '../../interfaces'
 
-const EditorWrapper = styled.div`
+const EditorLayout = styled.div`
+  display: grid;
   height: 100%;
-  padding: 1rem 0;
-
   background-color: ${props => props.theme.bg};
+
+  grid-template-columns: auto;
+  grid-template-rows: 4.4rem auto;
+  grid-template-areas: 'command-bar' 'editor';
+`
+
+const EditorWrapper = styled.div`
+  grid-area: editor;
+  height: 100%;
+
+  padding: 1rem 0;
+`
+
+const CommandBar = styled.div`
+  grid-area: command-bar;
+  height: 100%;
+
+  background-color: ${props => props.theme.darkAccent};
 `
 
 interface IProps {
@@ -23,7 +40,30 @@ interface IProps {
   editorValue: string
 }
 
+const editorOptions = {
+  selectOnLineNumbers: true,
+  scrollBeyondLastLine: false,
+}
+
 class Editor extends React.Component<IProps> {
+  editor
+  resizeListener
+
+  editorDidMount(editor, monaco) {
+    editor.focus()
+  }
+
+  handleEditorDidMount = editor => (this.editor = editor)
+  handleResize = () => this.editor.layout()
+
+  componentDidMount() {
+    this.resizeListener = window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeListener)
+  }
+
   updateValue = newValue =>
     this.props.updateSnippet(
       this.props.snippet.id,
@@ -34,14 +74,19 @@ class Editor extends React.Component<IProps> {
   render() {
     const { editorValue } = this.props
     return (
-      <EditorWrapper>
-        <MonacoEditor
-          theme="vs-dark"
-          language="typescript"
-          value={editorValue}
-          onChange={this.updateValue}
-        />
-      </EditorWrapper>
+      <EditorLayout>
+        <CommandBar>asdf</CommandBar>
+        <EditorWrapper>
+          <MonacoEditor
+            theme="vs-dark"
+            language="typescript"
+            value={editorValue}
+            options={editorOptions}
+            onChange={this.updateValue}
+            editorDidMount={this.handleEditorDidMount}
+          />
+        </EditorWrapper>
+      </EditorLayout>
     )
   }
 }
