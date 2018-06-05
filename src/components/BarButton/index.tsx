cb31037828a324cb55179bb2acac69d4835eda3f
Nico Bellante <nicobell@microsoft.com>
2018-06-05 12:30:14 -0700

    cleaned up react imports

--- src/components/BarButton/index.tsx
+++ src/components/BarButton/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 const BarButton = styled.div`
--- src/components/Editor/MonacoEditor.tsx
+++ src/components/Editor/MonacoEditor.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-// import * as monaco from 'monaco-editor'
-
 interface IMonacoOptions {
   selectOnLineNumbers: boolean
   scrollBeyondLastLine: boolean
