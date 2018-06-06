071cca78946c2c4b52de6debafeb40f4ba95a153
Nico Bellante <nicobell@microsoft.com>
2018-06-06 12:33:38 -0700

    deleted some unused utils and started a different redux approach

--- src/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { loadMonaco } from './loadMonaco';
--- src/utils/loadMonaco.ts
+++
@@ -1,9 +0,0 @@
-export function loadMonaco(): Promise<void> {
-  return new Promise(resolve => {
-    const require = (window as any).require
-
-    require.config({ paths: { vs: '../vs' } })
-
-    require(['vs/editor/editor.main'], resolve)
-  })
-}
