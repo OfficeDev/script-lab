23598beef05c7f93eb791f5b9915e2f60b9fe50b
Nico Bellante <nicobell@microsoft.com>
2018-06-22 15:39:47 -0700

    refactor out boilerplate files + solutions

--- src/actions/solutions.ts
+++ src/actions/solutions.ts
@@ -2,6 +2,7 @@ import { createAction } from 'typesafe-actions'
 import uuidv4 from 'uuid'
 import { add as addFiles } from './files'
 import { push } from 'connected-react-router'
+import { getBoilerplateSolution, getBoilerplateFiles } from '../newSolutionData'
 
 export const add = createAction('SOLUTIONS_ADD', resolve => {
   return (solution: ISolution) => resolve(solution)
@@ -17,42 +18,10 @@ export const remove = createAction('SOLUTIONS_REMOVE', resolve => {
 })
 
 export const create = () => dispatch => {
-  const now = Date.now()
-
-  const files = [
-    {
-      name: 'index.ts',
-      language: 'TypeScript',
-      content: `// hello world ${new Date().toUTCString()}\n`,
-    },
-    {
-      name: 'index.html',
-      language: 'HTML',
-      content: '<div>hello world</div>\n',
-    },
-    {
-      name: 'index.css',
-      language: 'CSS',
-      content: 'div {\n\tbackground-color: #333\n}\n',
-    },
-  ].map(file => ({
-    ...file,
-    id: uuidv4(),
-    dateCreated: now,
-    dateLastModified: now,
-  }))
-
-  const solutionId = uuidv4()
-
-  const solution: ISolution = {
-    id: solutionId,
-    name: `New Snippet`,
-    dateCreated: now,
-    dateLastModified: now,
-    files: files.map(file => file.id),
-  }
+  const files = getBoilerplateFiles()
+  const solution = getBoilerplateSolution(files)
 
   dispatch(addFiles(files))
   dispatch(add(solution))
-  dispatch(push(`/edit/${solutionId}`))
+  dispatch(push(`/edit/${solution.id}`))
 }
--- src/localStorage.ts
+++ src/localStorage.ts
@@ -1,8 +1,23 @@
+import { getBoilerplateSolution, getBoilerplateFiles } from './newSolutionData'
+
 export const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state')
     if (serializedState === null) {
-      return undefined
+      const files = getBoilerplateFiles()
+      const solution = getBoilerplateSolution(files)
+      return {
+        solutions: {
+          byId: {
+            [solution.id]: solution,
+          },
+          allIds: [solution.id],
+        },
+        files: {
+          byId: files.reduce((byIdFiles, file) => ({ ...byIdFiles, [file.id]: file })),
+          allIds: files.map(file => file.id),
+        },
+      }
     }
     return JSON.parse(serializedState)
   } catch (err) {
