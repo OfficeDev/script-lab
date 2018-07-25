bca6ceaaf5af68e438a7a5f381ee9cb0bc81a67e
Nico Bellante <nicobell@microsoft.com>
2018-07-25 14:14:39 -0700

    updated the 'new' solution (hced for excel atm)

--- src/newSolutionData.ts
+++ src/newSolutionData.ts
@@ -5,13 +5,45 @@ export const getBoilerplateFiles = (timestamp: number): IFile[] =>
     {
       name: 'index.ts',
       language: 'TypeScript',
-      content: `// hello world ${new Date().toUTCString()}\n`,
+      content: `$("#run").click(() => tryCatch(run));
+
+async function run() {
+    await Excel.run(async (context) => {
+        const range = context.workbook.getSelectedRange();
+        range.format.fill.color = "yellow";
+        range.load("address");
+
+        await context.sync()
+
+        console.log(\`The range address was "\${range.address}".\`);
+    });
+}
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+    try {
+        await callback();
+    }
+    catch (error) {
+        OfficeHelpers.UI.notify(error);
+        OfficeHelpers.Utilities.log(error);
+    }
+}
+`,
     },
-    { name: 'index.html', language: 'HTML', content: '<div>hello world</div>\n' },
+    {
+      name: 'index.html',
+      language: 'HTML',
+      content: `<p class="ms-font-m">Executes a simple code snippet.</p>
+<button id="run" class="ms-Button">
+    <span class="ms-Button-label">Run code</span>
+</button>
+`,
+    },
     {
       name: 'index.css',
       language: 'CSS',
-      content: 'div {\n\tbackground-color: #333\n}\n',
+      content: '',
     },
   ].map(file => ({
     ...file,
@@ -23,11 +55,26 @@ export const getBoilerplateFiles = (timestamp: number): IFile[] =>
 export const getBoilerplateSolution = (files: IFile[], timestamp: number): ISolution => ({
   id: uuidv4(),
   name: `Blank Snippet`,
-  host: 'WEB',
+  host: 'EXCEL',
   dateCreated: timestamp,
   dateLastModified: timestamp,
   files: files.map(file => file.id),
-  libraries: [],
+  libraries: [
+    'https://appsforoffice.microsoft.com/lib/1/hosted/office.js',
+    'https://appsforoffice.microsoft.com/lib/1/hosted/office.d.ts',
+
+    'office-ui-fabric-js@1.4.0/dist/css/fabric.min.css',
+    'office-ui-fabric-js@1.4.0/dist/css/fabric.components.min.css',
+
+    'core-js@2.4.1/client/core.min.js',
+    '@types/core-js',
+
+    '@microsoft/office-js-helpers@0.7.4/dist/office.helpers.min.js',
+    '@microsoft/office-js-helpers@0.7.4/dist/office.helpers.d.ts',
+
+    'jquery@3.1.1',
+    '@types/jquery',
+  ],
 })
 
 export const getBoilerplate = (): { solution: ISolution; files: IFile[] } => {
