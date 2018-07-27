44be9d150fe005c09be0a414256e580b4f48b925
Nico Bellante <nicobell@microsoft.com>
2018-07-27 13:00:40 -0700

    hmm

---
+++ server/devkeys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  github: {
+    clientId: '210a167954d9ef04b501',
+    clientSecret: '1facde921be3ffbecf7f9fbe73561aae73f43d04',
+    redirectUri: 'https://localhost:3000/',
+  },
+};
