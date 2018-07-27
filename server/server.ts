baba912951b8aa444cc88e2b20ca1c5c5626c9e7
Nico Bellante <nicobell@microsoft.com>
2018-07-27 12:56:36 -0700

    factored out keys, still need to put prodkeys on server

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const request = require('request');
+
+const keys = require('./keys');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// config
+app.use(cors());
+app.use(bodyParser.json());
+
+// routes
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
 app.post('/auth', (req, res) => {
+  console.log(req.body);
   const { code, state } = req.body;
   request.post(
     {
@@ -17,9 +27,9 @@ app.post('/auth', (req, res) => {
         Accept: 'application/json',
       },
       json: {
-        client_id: clientId,
-        client_secret: getClientSecret(),
-        redirect_uri: editorUrl,
+        client_id: keys.github.clientId,
+        client_secret: keys.github.clientSecret,
+        redirect_uri: keys.github.redirectUri,
         code,
         state,
       },
@@ -29,13 +39,13 @@ app.post('/auth', (req, res) => {
         console.error('github login failed');
         res
           .contentType('application/json')
-          .status(200)
-          .send(body);
+          .status(500)
+          .send({ error });
       } else {
         res
           .contentType('application/json')
-          .status(500)
-          .send({ error });
+          .status(200)
+          .send(body);
       }
     },
   );
