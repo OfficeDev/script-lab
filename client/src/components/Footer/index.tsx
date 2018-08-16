d30dd9883cfeb37b54f5a8b929f94618cb3511b1
nico bellante <nicobell@microsoft.com>
2018-08-15 23:25:40 -0700

    Footer cleanup and implementation of environment switcher

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { withTheme } from 'styled-components'
 
+import {
+  DirectionalHint,
+  ContextualMenuItemType,
+} from 'office-ui-fabric-react/lib/ContextualMenu'
+import { DefaultButton, IconButton } from 'office-ui-fabric-react/lib/Button'
+import { Customizer } from 'office-ui-fabric-react/lib/Utilities'
+import { createTheme } from 'office-ui-fabric-react/lib/Styling'
+
+import { HostType } from '@microsoft/office-js-helpers'
+
+import Only from '../Only'
 import { Wrapper } from './styles'
 import BarButton from '../BarButton'
 import FabricIcon from '../FabricIcon'
@@ -12,13 +24,94 @@ const languageMap = {
   json: 'JSON',
 }
 
-interface IFooter {
+export interface IFooterPropsFromRedux {
   language: string
-  onSettingsIconClick: () => void
+  currentHost: string
+  isWeb: boolean
 }
 
-const Footer = ({ language, onSettingsIconClick }: IFooter) => (
+export interface IFooterActionsFromRedux {
+  onSettingsIconClick: () => void
+  changeHost: (host: string) => void
+}
+
+export interface IFooter extends IFooterPropsFromRedux, IFooterActionsFromRedux {
+  activeFile: IFile
+  theme: any
+}
+
+const Footer = ({
+  language,
+  theme,
+  currentHost,
+  isWeb,
+  onSettingsIconClick,
+  changeHost,
+}: IFooter) => (
   <Wrapper>
+    <Only when={isWeb}>
+      <Customizer
+        settings={{
+          theme: createTheme({
+            palette: {
+              themePrimary: theme.primary,
+              themeSecondary: theme.primary,
+              themeDarkAlt: theme.primaryDark,
+              themeDark: theme.primaryDark,
+              themeTertiary: 'pink',
+              themeLight: 'pink',
+              themeLighter: 'magenta',
+              themeLighterAlt: 'orange',
+            },
+          }),
+        }}
+      >
+        <DefaultButton
+          primary={true}
+          text={currentHost}
+          // TODO(nicobell): fix the hover color
+          menuProps={{
+            isBeakVisible: true,
+            shouldFocusOnMount: true,
+            items: Object.keys(HostType)
+              .map(k => HostType[k])
+              .filter(v => v !== currentHost)
+              .map(v => ({
+                key: v,
+                text: v,
+                onClick: () => changeHost(v),
+                style: { color: theme.white },
+              })),
+            styles: props => ({
+              root: {
+                backgroundColor: theme.primary,
+                color: theme.white,
+              },
+            }),
+          }}
+          styles={{
+            root: {
+              height: '100%',
+              backgroundColor: theme.primary,
+              color: theme.white, // for the carrot
+              selectors: {
+                ':hover': {
+                  backgroundColor: `${theme.primaryDark} !important`,
+                  color: theme.white,
+                },
+                ':active': {
+                  backgroundColor: theme.primaryDark,
+                  color: theme.white,
+                },
+              },
+            },
+            label: {
+              color: theme.white,
+            },
+          }}
+        />
+      </Customizer>
+    </Only>
     {languageMap[language.toLowerCase()] && (
       <BarButton>{languageMap[language.toLowerCase()]}</BarButton>
     )}
@@ -28,4 +121,4 @@ const Footer = ({ language, onSettingsIconClick }: IFooter) => (
   </Wrapper>
 )
 
-export default Footer
+export default withTheme(Footer)
