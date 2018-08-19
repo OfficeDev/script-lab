c644f32b2b365d8bcc07281092002a3d0df51c6b
nico bellante <nicobell@microsoft.com>
2018-08-18 20:30:41 -0700

    actions alphabetize

--- client/src/store/actions.ts
+++ client/src/store/actions.ts
@@ -1,35 +1,39 @@
 import { ActionType } from 'typesafe-actions'
 
-import * as solutions from './solutions/actions'
-import * as host from './host/actions'
 import * as gists from './gists/actions'
-import * as samples from './samples/actions'
 import * as github from './github/actions'
+import * as host from './host/actions'
 import * as messageBar from './messageBar/actions'
+import * as misc from './misc/actions'
+import * as samples from './samples/actions'
 import * as settings from './settings/actions'
+import * as solutions from './solutions/actions'
 
-export { solutions }
-export { host }
 export { gists }
-export { samples }
-export { messageBar }
-export { settings }
 export { github }
+export { host }
+export { messageBar }
+export { misc }
+export { samples }
+export { settings }
+export { solutions }
 
 export default {
-  solutions,
-  host,
   gists,
-  samples,
   github,
+  host,
   messageBar,
+  misc,
+  samples,
   settings,
+  solutions,
 }
 
-export type ISolutionsAction = ActionType<typeof solutions>
-export type IHostAction = ActionType<typeof host>
 export type IGistsAction = ActionType<typeof gists>
-export type ISamplesAction = ActionType<typeof samples>
 export type IGithubAction = ActionType<typeof github>
+export type IHostAction = ActionType<typeof host>
 export type IMessageBarAction = ActionType<typeof messageBar>
+export type IMiscAction = ActionType<typeof misc>
+export type ISamplesAction = ActionType<typeof samples>
 export type ISettingsAction = ActionType<typeof settings>
+export type ISolutionsAction = ActionType<typeof solutions>
