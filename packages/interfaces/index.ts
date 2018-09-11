c43ac730878849b14e92afa185417247b34813c1
Nico Bellante <nicobell@microsoft.com>
2018-09-11 15:48:04 -0700

    basic runner setup with shared interface files

---
+++ packages/interfaces/index.ts
@@ -0,0 +1,29 @@
+// script-lab
+interface IContentLanguagePair {
+  content: string;
+  language: string;
+}
+
+interface ISnippet {
+  id: string;
+  gist?: string;
+  gistOwnerId?: string;
+  name: string;
+  description?: string;
+  /** author: export-only */
+  author?: string;
+  host: string;
+  /** api_set: export-only (+ check at first level of import) */
+  api_set?: {
+    [index: string]: number;
+  };
+  platform: string;
+  created_at: number;
+  modified_at: number;
+  order?: number;
+
+  script: IContentLanguagePair;
+  template: IContentLanguagePair;
+  style: IContentLanguagePair;
+  libraries: string;
+}
