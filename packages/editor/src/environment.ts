8021f6e46ad873f8e869ce120ea2742bf58d3255
Nico Bellante <nicobell@microsoft.com>
2018-09-20 17:16:58 -0700

    strange..

--- packages/editor/src/environment.ts
+++ packages/editor/src/environment.ts
@@ -1,5 +1,5 @@
 export const authServerUrl = {
-  local: 'https://localhost:5000',
+  local: 'http://localhost:5000',
   alpha: 'https://script-lab-react-server-alpha.azurewebsites.net',
   beta: 'https://script-lab-react-server-beta.azurewebsites.net',
   prod: 'https://script-lab-react-server.azurewebsites.net',
