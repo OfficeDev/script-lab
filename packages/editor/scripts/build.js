241c7917f3e7f7a7a062cefe7a663d6d47a025e9
nico bellante <nicobell@microsoft.com>
2018-09-22 00:16:19 -0700

    base staging for editor on location.origin

--- packages/editor/scripts/build.js
+++ packages/editor/scripts/build.js
@@ -1,15 +1,8 @@
 var shell = require('shelljs')
 
-var { TRAVIS_BRANCH, TRAVIS_COMMIT } = process.env // from travis
-
-var REACT_APP_STAGING = {
-  master: 'alpha',
-  beta: 'beta',
-  production: 'production',
-}[TRAVIS_BRANCH]
+var { TRAVIS_COMMIT } = process.env // from travis
 
 var commands = [
-  `export REACT_APP_STAGING='${REACT_APP_STAGING}'`,
   `export REACT_APP_COMMIT='${TRAVIS_COMMIT}'`,
   `export REACT_APP_LAST_UPDATED='${new Date().toUTCString()}'`,
   'yarn react-scripts-ts:build',
--- packages/editor/src/environment.ts
+++ packages/editor/src/environment.ts
@@ -3,21 +3,21 @@ export const authServerUrl = {
   alpha: 'https://script-lab-react-server-alpha.azurewebsites.net',
   beta: 'https://script-lab-react-server-beta.azurewebsites.net',
   prod: 'https://script-lab-react-server.azurewebsites.net',
-}[process.env.REACT_APP_STAGING || 'prod']
+}[getCurrentEnv()]
 
 export const githubAppClientId = {
   local: '210a167954d9ef04b501',
   alpha: 'ad26df7ba62ef691669e',
   beta: 'edb61fe543b382628d68',
   prod: '5db9e6dbe957707a3cb2',
-}[process.env.REACT_APP_STAGING || 'prod']
+}[getCurrentEnv()]
 
 export const environmentName = {
   local: 'local',
   alpha: 'react-alpha',
   beta: 'react-beta',
   prod: 'react',
-}[process.env.REACT_APP_STAGING || 'prod']
+}[getCurrentEnv()]
 
 export const editorUrls = {
   local: 'https://localhost:3000',
@@ -26,4 +26,13 @@ export const editorUrls = {
   alpha: 'https://bornholm-edge.azurewebsites.net',
   beta: 'https://bornholm-insiders.azurewebsites.net',
   production: 'https://script-lab.azureedge.net',
+}
+
+function getCurrentEnv(): 'local' | 'alpha' | 'beta' | 'production' {
+  return {
+    'https://localhost:3000': 'local',
+    'https://script-lab-react-alpha.azurewebsites.net': 'alpha',
+    'https://script-lab-react-beta.azurewebsites.net': 'beta',
+    'https://script-lab-react.azurewebsites.net': 'production',
+  }[window.location.origin]
 }
