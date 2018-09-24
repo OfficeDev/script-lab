8935007d111ee8040ccef7d2bd26dbb71b4c28d8
Nico Bellante <nicobell@microsoft.com>
2018-09-24 13:36:24 -0700

    remaining pre pr changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,20 @@
-var shell = require('shelljs');
+var shell = require("shelljs");
 
 var {
-  TRAVIS,
   TRAVIS_BRANCH,
-  TRAVIS_PULL_REQUEST,
   TRAVIS_COMMIT_MESSAGE,
   SITE_NAME,
   PACKAGE_LOCATION,
   DEPLOYMENT_USERNAME,
-  DEPLOYMENT_PASSWORD,
+  DEPLOYMENT_PASSWORD
 } = process.env; // from travis
 
-var TRAVIS_COMMIT_MESSAGE_SANITIZED = TRAVIS_COMMIT_MESSAGE.replace(/\W/g, '_');
+var TRAVIS_COMMIT_MESSAGE_SANITIZED = TRAVIS_COMMIT_MESSAGE.replace(/\W/g, "_");
 
 var deploymentSlot = {
-  deployment: '-alpha',
-  beta: '-beta',
-  production: '',
+  deployment: "-alpha",
+  beta: "-beta",
+  production: ""
 }[TRAVIS_BRANCH];
 
 var SITE = `${SITE_NAME}${deploymentSlot}`;
@@ -25,16 +23,15 @@ if (deploymentSlot !== undefined) {
   shell.exec('echo "starting deployment"');
   shell.cd(`${PACKAGE_LOCATION}/build`);
 
-  shell.exec('git init');
+  shell.exec("git init");
 
   shell.exec('git config --add user.name "Travis CI"');
   shell.exec('git config --add user.email "travis.ci@microsoft.com"');
 
-  shell.exec('git add -A');
+  shell.exec("git add -A");
   shell.exec(`git commit -m "${TRAVIS_COMMIT_MESSAGE_SANITIZED}"`);
 
   var result = shell.exec(
-    `git push https://${DEPLOYMENT_USERNAME}:${DEPLOYMENT_PASSWORD}@${SITE}.scm.azurewebsites.net:443/${SITE_NAME}.git -q -f -u HEAD:refs/heads/master`,
-    // { silent: true },
+    `git push https://${DEPLOYMENT_USERNAME}:${DEPLOYMENT_PASSWORD}@${SITE}.scm.azurewebsites.net:443/${SITE_NAME}.git -q -f -u HEAD:refs/heads/master`
   );
 }
