1c0663572ed9d92f241c9514159c3abf6651f8be
nico bellante <nicobell@microsoft.com>
2018-09-23 23:26:24 -0700

    test custom deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,40 @@
+var shell = require('shelljs');
+
+var {
+  TRAVIS,
+  TRAVIS_BRANCH,
+  TRAVIS_PULL_REQUEST,
+  TRAVIS_COMMIT_MESSAGE,
+  SITE_NAME,
+  PACKAGE_LOCATION,
+  DEPLOYMENT_USERNAME,
+  DEPLOYMENT_PASSWORD,
+} = process.env; // from travis
+
+var TRAVIS_COMMIT_MESSAGE_SANITIZED = TRAVIS_COMMIT_MESSAGE.replace(/\W/g, '_');
+
+var deploymentSlot = {
+  deployment: '-alpha',
+  beta: '-beta',
+  production: '',
+}[TRAVIS_BRANCH];
+
+var SITE = `${SITE_NAME}${deploymentSlot}`;
+
+if (deploymentSlot !== undefined) {
+  shell.exec('echo "starting deployment"');
+  shell.cd(`${PACKAGE_LOCATION}/build`);
+
+  shell.exec('git init');
+
+  shell.exec('git config --add user.name "Travis CI"');
+  shell.exec('git config --add user.email "travis.ci@microsoft.com"');
+
+  shell.exec('git add -A');
+  shell.exec(`git commit -m "${TRAVIS_COMMIT_MESSAGE_SANITIZED}"`);
+
+  var result = shell.exec(
+    `git push https://${DEPLOYMENT_USERNAME}:${DEPLOYMENT_PASSWORD}@${SITE}.scm.azurewebsites.net:443/${SITE_NAME}.git -q -f -u HEAD:refs/heads/master`,
+    // { silent: true },
+  );
+}
