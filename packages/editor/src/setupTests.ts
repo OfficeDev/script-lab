44e51bdc2ed026f8971f621e33713159bc45676d
Nico Bellante <nicobell@microsoft.com>
2018-09-25 13:13:45 -0700

    added jest-styled-components and updated snapshots

--- packages/editor/src/setupTests.ts
+++ packages/editor/src/setupTests.ts
@@ -1,6 +1,9 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
+import 'jest-styled-components'
+
+
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() })
 
