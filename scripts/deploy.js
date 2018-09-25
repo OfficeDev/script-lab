8339e6127266b6dc311f5afee15718f118f0e1dc
nico bellante <nicobell@microsoft.com>
2018-09-25 00:56:39 -0700

    branch whitelist for travis

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-var shell = require("shelljs");
+var shell = require('shelljs');
 
 var {
   TRAVIS_BRANCH,
@@ -6,15 +6,15 @@ var {
   SITE_NAME,
   PACKAGE_LOCATION,
   DEPLOYMENT_USERNAME,
-  DEPLOYMENT_PASSWORD
+  DEPLOYMENT_PASSWORD,
 } = process.env; // from travis
 
-var TRAVIS_COMMIT_MESSAGE_SANITIZED = TRAVIS_COMMIT_MESSAGE.replace(/\W/g, "_");
+var TRAVIS_COMMIT_MESSAGE_SANITIZED = TRAVIS_COMMIT_MESSAGE.replace(/\W/g, '_');
 
 var deploymentSlot = {
-  deployment: "-alpha",
-  beta: "-beta",
-  production: ""
+  deployment: '-alpha', // TODO: change deployment to master
+  beta: '-beta',
+  production: '',
 }[TRAVIS_BRANCH];
 
 var SITE = `${SITE_NAME}${deploymentSlot}`;
@@ -23,15 +23,15 @@ if (deploymentSlot !== undefined) {
   shell.exec('echo "starting deployment"');
   shell.cd(`${PACKAGE_LOCATION}/build`);
 
-  shell.exec("git init");
+  shell.exec('git init');
 
   shell.exec('git config --add user.name "Travis CI"');
   shell.exec('git config --add user.email "travis.ci@microsoft.com"');
 
-  shell.exec("git add -A");
+  shell.exec('git add -A');
   shell.exec(`git commit -m "${TRAVIS_COMMIT_MESSAGE_SANITIZED}"`);
 
   var result = shell.exec(
-    `git push https://${DEPLOYMENT_USERNAME}:${DEPLOYMENT_PASSWORD}@${SITE}.scm.azurewebsites.net:443/${SITE_NAME}.git -q -f -u HEAD:refs/heads/master`
+    `git push https://${DEPLOYMENT_USERNAME}:${DEPLOYMENT_PASSWORD}@${SITE}.scm.azurewebsites.net:443/${SITE_NAME}.git -q -f -u HEAD:refs/heads/master`,
   );
 }
