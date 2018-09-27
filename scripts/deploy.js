9ea2dcb80a26fed77f54cee5849b2c8e57cfbdd6
Nico Bellante <nicobell@microsoft.com>
2018-09-27 12:19:24 -0700

    deployment script changes + test + CR feedback

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-var shell = require('shelljs');
+var shell = require("shelljs");
 
 var {
   TRAVIS_BRANCH,
@@ -6,47 +6,56 @@ var {
   SITE_NAME,
   PACKAGE_LOCATION,
   DEPLOYMENT_USERNAME,
-  DEPLOYMENT_PASSWORD,
+  DEPLOYMENT_PASSWORD
 } = process.env; // from travis
 
-var TRAVIS_COMMIT_MESSAGE_SANITIZED = TRAVIS_COMMIT_MESSAGE.replace(/\W/g, '_');
+var TRAVIS_COMMIT_MESSAGE_SANITIZED = TRAVIS_COMMIT_MESSAGE.replace(/\W/g, "_");
 
 var deploymentSlot = {
-  master: '-alpha',
-  beta: '-beta',
-  production: '',
+  "storybook-deployment": "-alpha",
+  beta: "-beta",
+  production: ""
 }[TRAVIS_BRANCH];
 
 if (deploymentSlot !== undefined) {
-  if (shell.test('-d', `${PACKAGE_LOCATION}/build`)) {
-    shell.exec('echo "starting deployment"');
-    deploy(`${PACKAGE_LOCATION}/build`, SITE_NAME, `${SITE_NAME}${deploymentSlot}`);
+  if (shell.test("-d", `${PACKAGE_LOCATION}/build`)) {
+    shell.echo("starting deployment");
+    deploy(
+      `${PACKAGE_LOCATION}/build`,
+      SITE_NAME,
+      `${SITE_NAME}${deploymentSlot}`
+    );
+  } else {
+    shell.echo("ERROR: No build directory found!");
+    shell.exit(1);
   }
 
-  if (shell.test('-d', './build-storybook')) {
-    shell.exec('echo "starting deployment of storybook"');
+  // Not all packages have a storybook, therefore only
+  // deploy storybook when one exists
+  if (shell.test("-d", `${PACKAGE_LOCATION}/build-storybook`)) {
+    shell.echo("starting deployment of storybook");
     deploy(
       `./build-storybook`,
       `${SITE_NAME}-storybook`,
-      `${SITE_NAME}-storybook${deploymentSlot}`,
+      `${SITE_NAME}-storybook${deploymentSlot}`
     );
   }
 }
 
 function deploy(path, site, siteWithStaging) {
-  shell.cd(path);
+  shell.pushd(path);
 
-  shell.exec('git init');
+  shell.exec("git init");
 
   shell.exec('git config --add user.name "Travis CI"');
   shell.exec('git config --add user.email "travis.ci@microsoft.com"');
 
-  shell.exec('git add -A');
+  shell.exec("git add -A");
   shell.exec(`git commit -m "${TRAVIS_COMMIT_MESSAGE_SANITIZED}"`);
 
   var result = shell.exec(
-    `git push https://${DEPLOYMENT_USERNAME}:${DEPLOYMENT_PASSWORD}@${siteWithStaging}.scm.azurewebsites.net:443/${site}.git -q -f -u HEAD:refs/heads/master`,
+    `git push https://${DEPLOYMENT_USERNAME}:${DEPLOYMENT_PASSWORD}@${siteWithStaging}.scm.azurewebsites.net:443/${site}.git -f -u HEAD:refs/heads/master`
   );
 
-  shell.cd('..');
+  shell.popd();
 }
