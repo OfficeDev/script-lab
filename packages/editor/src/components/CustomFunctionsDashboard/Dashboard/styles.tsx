58aaee8b1e781425e204c946749e3211702da555
Nico Bellante <nicobell@microsoft.com>
2018-09-27 11:41:20 -0700

    added whitespace

--- packages/editor/src/components/CustomFunctionsDashboard/Dashboard/styles.tsx
+++ packages/editor/src/components/CustomFunctionsDashboard/Dashboard/styles.tsx
@@ -5,10 +5,12 @@ export const Layout = styled.div`
   flex-direction: column;
   height: 100vh;
 `
+
 export const Header = styled.header`
   box-shadow: 0px 2px 5px 2px ${props => props.theme.neutralSecondary};
   z-index: 1000;
 `
+
 export const Content = styled.div`
   flex: 1;
   overflow: hidden;
