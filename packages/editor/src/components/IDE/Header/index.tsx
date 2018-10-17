79fe32cfddfabdd64b2f0c7a4434c1c807b93338
Nico Bellante <nicobell@microsoft.com>
2018-10-17 12:40:45 -0700

    fixes #135

--- packages/editor/src/components/IDE/Header/index.tsx
+++ packages/editor/src/components/IDE/Header/index.tsx
@@ -5,6 +5,7 @@ import { Customizer } from 'office-ui-fabric-react/lib/Utilities'
 import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar'
 import { PersonaSize, PersonaCoin } from 'office-ui-fabric-react/lib/Persona'
 import { MessageBarType } from 'office-ui-fabric-react/lib/MessageBar'
+import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner'
 
 import Clipboard from 'clipboard'
 import { convertSolutionToSnippet } from '../../../utils'
@@ -12,12 +13,7 @@ import YAML from 'js-yaml'
 
 import SolutionSettings from './SolutionSettings'
 import { ITheme as IFabricTheme } from 'office-ui-fabric-react/lib/Styling'
-import {
-  NULL_SOLUTION_ID,
-  SETTINGS_SOLUTION_ID,
-  PATHS,
-  IS_TASK_PANE_WIDTH,
-} from '../../../constants'
+import { NULL_SOLUTION_ID, PATHS, IS_TASK_PANE_WIDTH } from '../../../constants'
 import { getPlatform, PlatformType } from '../../../environment'
 
 import { connect } from 'react-redux'
@@ -33,11 +29,12 @@ const HeaderWrapper = styled.header`
 `
 
 interface IPropsFromRedux {
-  profilePicUrl?: string
+  profilePicUrl: string | null
   isRunnableOnThisHost: boolean
   isSettingsView: boolean
   isCustomFunctionsView: boolean
   isLoggedIn: boolean
+  isLoggingInOrOut: boolean
   commandBarFabricTheme: IFabricTheme
   screenWidth: number
 }
@@ -46,6 +43,7 @@ const mapStateToProps = (state): IPropsFromRedux => ({
   isSettingsView: selectors.settings.getIsOpen(state),
   isCustomFunctionsView: selectors.customFunctions.getIsCurrentSolutionCF(state),
   isLoggedIn: !!selectors.github.getToken(state),
+  isLoggingInOrOut: selectors.github.getIsLoggingInOrOut(state),
   isRunnableOnThisHost: selectors.host.getIsRunnableOnThisHost(state),
   profilePicUrl: selectors.github.getProfilePicUrl(state),
   commandBarFabricTheme: getCommandBarFabricTheme(selectors.host.get(state)),
@@ -143,6 +141,7 @@ class HeaderWithoutTheme extends React.Component<IProps, IState> {
       profilePicUrl,
       isRunnableOnThisHost,
       isLoggedIn,
+      isLoggingInOrOut,
       screenWidth,
       theme,
       commandBarFabricTheme,
@@ -280,16 +279,20 @@ class HeaderWithoutTheme extends React.Component<IProps, IState> {
       key: 'account',
       onRenderIcon: () => (
         <div style={{ width: '28px', overflow: 'hidden' }}>
-          <PersonaCoin
-            imageUrl={profilePicUrl}
-            size={PersonaSize.size28}
-            initialsColor="white"
-            styles={{
-              initials: {
-                color: (theme && theme.primary) || 'black',
-              },
-            }}
-          />
+          {isLoggingInOrOut ? (
+            <Spinner size={SpinnerSize.medium} />
+          ) : (
+            <PersonaCoin
+              imageUrl={profilePicUrl || undefined}
+              size={PersonaSize.size28}
+              initialsColor="white"
+              styles={{
+                initials: {
+                  color: (theme && theme.primary) || 'black',
+                },
+              }}
+            />
+          )}
         </div>
       ),
       ariaLabel: isLoggedIn ? 'Logout' : 'Login',
@@ -305,7 +308,7 @@ class HeaderWithoutTheme extends React.Component<IProps, IState> {
           }
         : undefined,
       iconOnly: true,
-      onClick: login,
+      onClick: isLoggingInOrOut ? () => {} : login,
     }
 
     return (
--- packages/editor/src/store/github/selectors.ts
+++ packages/editor/src/store/github/selectors.ts
@@ -1,5 +1,9 @@
 import { IState } from '../reducer'
 
-export const getToken = (state: IState): string | undefined => state.github.token
-export const getProfilePicUrl = (state: IState): string | undefined =>
+export const getToken = (state: IState): string | null => state.github.token
+
+export const getProfilePicUrl = (state: IState): string | null =>
   state.github.profilePicUrl
+
+export const getIsLoggingInOrOut = (state: IState): boolean =>
+  state.github.isLoggingInOrOut
