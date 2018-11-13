10e37d1981199551beee4812699312f0adb3d151
Nico Bellante <nicobell@microsoft.com>
2018-11-13 14:00:38 -0800

    clean up setupTests

--- packages/editor/src/setupTests.ts
+++ packages/editor/src/setupTests.ts
@@ -2,12 +2,9 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import 'jest-styled-components';
-// import * as mergeStylesSerializer from '@uifabric/jest-serializer-merge-styles';
 
 // React 16 Enzyme adapter
-Enzyme.configure({ adapter: new Adapter() });
-
-// expect.addSnapshotSerializer(mergeStylesSerializer);
+Enzyme.configure({ adapter: Adapter() });
 
 // TODO: figure out why this doesn't work
 // (having this commented out causes fabric to throw warnings for having uninitialized icons)
