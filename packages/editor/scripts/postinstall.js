e65d8c29ce75ea84bdd41f08d0a3e82d394e44ba
Michael Zlatkovsky <Zlatkovsky@users.noreply.github.com>
2018-11-16 10:21:16 -0800

    Minor updates

--- packages/editor/scripts/postinstall.js
+++ packages/editor/scripts/postinstall.js
@@ -1,4 +1,10 @@
 const fs = require('fs-extra');
+
+// Remove old files:
+if (fs.existsSync('./public/vs')) {
+  fs.removeSync('./public/vs');
+}
+
 const filesToCopy = [
   {
     from: '../../node_modules/monaco-editor/min/vs',
