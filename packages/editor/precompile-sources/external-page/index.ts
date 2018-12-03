f06300519d04c1fc57399c9fbdc2e6026fada493
Michael Zlatkovsky <michael.zlat@gmail.com>
2018-12-02 17:07:44 -0800

    Remove relience of lodash/forin (which for some reason caused build break) Could certainly be fixed, but not worth it.  I can just remove it. It even makes the code a little simpler.

--- packages/editor/precompile-sources/external-page/index.ts
+++ packages/editor/precompile-sources/external-page/index.ts
@@ -1,26 +1,18 @@
 import { parse } from 'query-string';
 import safeExternalUrls from '../../../common/src/safe.external.urls';
-import forIn from 'lodash/forin';
 
 (() => {
   const { destination } = parse(window.location.search) as {
     destination: string;
   };
 
-  forIn(
-    safeExternalUrls,
-    (value: string): any => {
-      if (value === destination) {
-        window.location.href = destination;
-        return false;
-      }
-    },
-  );
-
+  for (const key in safeExternalUrls) {
+    const value = (safeExternalUrls as { [key: string]: string })[key];
+    if (value === destination) {
+      window.location.href = destination;
+      return;
+    }
+  }
   // Otherwise can just stay on empty page.  This should never happen in normal behavior,
   // and if someone tweaks the URL, then so be it.
 })();
-
-///////////////////////////////////////
-
-// cspell:ignore forin
