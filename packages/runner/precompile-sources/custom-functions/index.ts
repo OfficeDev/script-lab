515e37100cab6262a9e7d4334eed585dd4b92875
Zlatkovsky <michael.zlat@gmail.com>
2018-12-06 16:38:56 -0800

    Basic custom functions on Excel Online with fake data working

--- packages/runner/precompile-sources/custom-functions/index.ts
+++ packages/runner/precompile-sources/custom-functions/index.ts
@@ -1,164 +1,163 @@
-// import { officeNamespacesForCustomFunctionsIframe } from '../../src/constants';
-// import { generateLogString, stringifyPlusPlus } from 'common/lib/utilities/string';
-// import 'core-js/fn/array/find';
+import { officeNamespacesForCustomFunctionsIframe } from '../../src/constants';
+import { generateLogString, stringifyPlusPlus } from 'common/lib/utilities/string';
+import 'core-js/fn/array/find';
 
-// (async () => {
-//   overwriteConsole('[SYSTEM]', window);
+import generateCustomFunctionIframe, {
+  ICustomFunctionPayload,
+} from './run.customFunctions';
 
-//   const payload = getFakePayload();
-//   await initializeRunnableSnippets(payload);
-// })();
-// ///////////////////////////////////////
+const VERBOSE_MODE = true; // FIXME: Nico: you'll probably want to turn this off before going to production
 
-// async function initializeRunnableSnippets(
-//   payload: Array<{
-//     solutionId: string;
-//     namespace: string;
-//     functionNames: string[];
-//     code: string;
-//     jsLibs: string[];
-//   }>,
-// ) {
-//   return new Promise(resolve =>
-//     tryCatch(() => {
-//       let successfulRegistrationsCount = 0;
+(async () => {
+  // FIXME: Start heartbeat and hook up refresh event.  Note, might not need refresh if reloading the taskpane (which calls "CustomFunctionsManager.register" destroys the old iframe)
 
-//       (window as any).scriptRunnerOnLoad = (contentWindow: Window) =>
-//         tryCatch(() => {
-//           (contentWindow as any).console = window.console;
-//           contentWindow.onerror = (...args) => console.error(args);
+  overwriteConsole('[SYSTEM]', window);
 
-//           officeNamespacesForCustomFunctionsIframe.forEach(namespace => {
-//             (contentWindow as any)[namespace] = (window as any)[namespace];
-//           });
-//         });
+  const payload = getFakePayload();
+  await initializeRunnableSnippets(payload);
+  logIfExtraLoggingEnabled('Done preparing snippets');
 
-//       (window as any).scriptRunnerEndInit = (iframeWindow: Window, id: string) =>
-//         tryCatch(() => {
-//           const snippetMetadata = payload.find(item => item.solutionId === id);
-//           const namespaceUppercase = snippetMetadata.namespace.toUpperCase();
+  // tslint:disable-next-line:no-string-literal
+  delete CustomFunctionMappings['__delay__'];
 
-//           logIfExtraLoggingEnabled(
-//             `Mapping custom functions from namespace ${
-//               snippetMetadata.namespace
-//             }, expecting ${snippetMetadata.functions.length} functions`,
-//           );
+  logIfExtraLoggingEnabled(
+    'Custom functions runner is ready to evaluate your functions!',
+  );
+})();
+///////////////////////////////////////
 
-//           snippetMetadata.functions.map(func => {
-//             const funcFullUpperName = `${namespaceUppercase}.${func.funcName.toUpperCase()}`;
+async function initializeRunnableSnippets(payload: ICustomFunctionPayload[]) {
+  return new Promise(resolve =>
+    tryCatch(() => {
+      let successfulRegistrationsCount = 0;
 
-//             // Overwrite console.log on every snippet iframe
-//             overwriteConsole(funcFullUpperName, iframeWindow);
-//           });
+      (window as any).scriptRunnerOnLoad = (contentWindow: Window, id: string) =>
+        tryCatch(() => {
+          const snippetMetadata = payload.find(item => item.solutionId === id)!;
+          overwriteConsole(snippetMetadata.namespace, contentWindow);
+          contentWindow.onerror = (...args) => console.error(args);
 
-//           successfulRegistrationsCount++;
+          logIfExtraLoggingEnabled(
+            `Snippet for namespace "${snippetMetadata.namespace}" beginning to load.`,
+          );
 
-//           if (successfulRegistrationsCount === metadataArray.length) {
-//             resolve();
-//           }
-//         });
+          officeNamespacesForCustomFunctionsIframe.forEach(namespace => {
+            (contentWindow as any)[namespace] = (window as any)[namespace];
+          });
+        });
 
-//       const snippetsHtmls: string[] = JSON.parse(atob(params.snippetsDataBase64));
+      (window as any).scriptRunnerOnLoadComplete = () => {
+        if (++successfulRegistrationsCount === payload.length) {
+          resolve();
+        }
+      };
 
-//       snippetsHtmls.forEach(html => {
-//         let $iframe = $(
-//           '<iframe class="snippet-frame" src="about:blank"></iframe>',
-//         ).appendTo('body');
-//         let iframe = $iframe[0] as HTMLIFrameElement;
-//         let { contentWindow } = iframe;
+      payload.forEach(customFuncData => {
+        const iframe = document.createElement('iframe');
+        iframe.src = 'about:blank';
+        document.head.insertBefore(iframe, null);
 
-//         // Write to the iframe (and note that must do the ".write" call first,
-//         // before setting any window properties). Setting console and onerror here
-//         // (for any initial logging or error handling from snippet-referenced libraries),
-//         // but for extra safety also setting them inside of scriptRunnerInitialized.
-//         contentWindow.document.open();
-//         contentWindow.document.write(html);
-//         (contentWindow as any).console = window.console;
-//         contentWindow.onerror = (...args) => {
-//           handleError({ error: args });
-//         };
-//         contentWindow.document.close();
-//       });
-//     }),
-//   );
-// }
+        const contentWindow = iframe.contentWindow!;
 
-// function overwriteConsole(source: '[SYSTEM]' | string, windowObject: Window) {
-//   const logTypes: ConsoleLogTypes[] = ['log', 'info', 'warn', 'error'];
-//   logTypes.forEach(
-//     methodName =>
-//       ((windowObject.console as any)[methodName] = consoleMsgTypeImplementation(
-//         methodName,
-//       )),
-//   );
+        // Write to the iframe (and note that must do the ".write" call first,
+        // before setting any window properties). Setting console and onerror here
+        // (for any initial logging or error handling from snippet-referenced libraries),
+        // but for extra safety also setting them inside of scriptRunnerOnLoad.
+        contentWindow.document.open();
+        contentWindow.document.write(generateCustomFunctionIframe(customFuncData));
+        (contentWindow as any).console = window.console;
+        contentWindow.onerror = (...args) => {
+          handleError({ error: args });
+        };
+        contentWindow.document.close();
+      });
+    }),
+  );
+}
 
-//   function consoleMsgTypeImplementation(severityType: ConsoleLogTypes) {
-//     return (...args: any[]) => {
-//       const { severity, message } = generateLogString(args, severityType);
+function logIfExtraLoggingEnabled(message: string) {
+  if (VERBOSE_MODE) {
+    tryToSendLog({
+      message: message,
+      severity: 'info',
+      source: '[SYSTEM]',
+    });
+  }
+}
 
-//       tryToSendLog({
-//         source,
-//         severity,
-//         message,
-//       });
-//     };
-//   }
-// }
+function overwriteConsole(source: '[SYSTEM]' | string, windowObject: Window) {
+  const logTypes: ConsoleLogTypes[] = ['log', 'info', 'warn', 'error'];
+  logTypes.forEach(
+    methodName =>
+      ((windowObject.console as any)[methodName] = consoleMsgTypeImplementation(
+        methodName,
+      )),
+  );
 
-// function tryToSendLog(data: { source: string; severity: string; message: string }) {
-//   try {
-//     // tslint:disable-next-line:no-debugger
-//     debugger;
-//     console.log(data);
-//   } catch (e) {
-//     // If couldn't log, not much you can do about it.  Write to console.log just in case,
-//     // but that's not going to help much on an invisible runner...
-//     console.log(e);
-//   }
-// }
+  function consoleMsgTypeImplementation(severityType: ConsoleLogTypes) {
+    return (...args: any[]) => {
+      const { severity, message } = generateLogString(args, severityType);
 
-// async function tryCatch(func: () => any) {
-//   try {
-//     await func();
-//   } catch (e) {
-//     handleError(e);
-//   }
-// }
+      tryToSendLog({
+        source,
+        severity,
+        message,
+      });
+    };
+  }
+}
 
-// function handleError(error: Error | any) {
-//   tryToSendLog({
-//     message: stringifyPlusPlus(error),
-//     severity: 'error',
-//     source: '[SYSTEM]',
-//   });
-// }
+function tryToSendLog(data: { source: string; severity: string; message: string }) {
+  try {
+    // FIXME: Nico: pass this to the heartbeat and also add a counter for the ID
+    debugger;
+  } catch (e) {
+    // If couldn't log, not much you can do about it.
+  }
+}
 
-// // FIXME: Zlatkovsky
-// function getFakePayload() {
-//   return [
-//     {
-//       solutionId: 'FooBar',
-//       namespace: 'FooBar',
-//       functionNames: ['foo', 'bar'],
-//       code:
-//         '/** @customFunction */\r\nfunction foo() {\r\n    return 42;\r\n}\r\n/** @customFunction */\r\nfunction bar() {\r\n    return 43;\r\n}\r\n',
-//       jsLibs: [
-//         'https://unpkg.com/core-js@2.4.1/client/core.min.js',
-//         'https://unpkg.com/@microsoft/office-js-helpers@0.7.4/dist/office.helpers.min.js',
-//         'https://unpkg.com/jquery@3.1.1',
-//       ],
-//     },
-//     {
-//       solutionId: 'asdfasdfasd2323423432',
-//       namespace: 'BlankSnippet',
-//       functionNames: ['add10', 'add5ish'],
-//       code:
-//         '/** @CustomFunction */\r\nfunction add10(x) {\r\n    return x + 10;\r\n}\r\n/** @CustomFunction */\r\nfunction add5ish(x) {\r\n    var num = Math.random();\r\n    console.log(num);\r\n    return x + num + 5;\r\n}\r\n',
-//       jsLibs: [
-//         'https://unpkg.com/core-js@2.4.1/client/core.min.js',
-//         'https://unpkg.com/@microsoft/office-js-helpers@0.7.4/dist/office.helpers.min.js',
-//         'https://unpkg.com/jquery@3.1.1',
-//       ],
-//     },
-//   ];
-// }
+async function tryCatch(func: () => any) {
+  try {
+    await func();
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+function handleError(error: Error | any) {
+  tryToSendLog({
+    message: stringifyPlusPlus(error),
+    severity: 'error',
+    source: '[SYSTEM]',
+  });
+}
+
+// FIXME: Nico: replace with actual data that comes from heartbeat
+function getFakePayload() {
+  return [
+    {
+      solutionId: 'FooBar',
+      namespace: 'FooBar',
+      functionNames: ['foo', 'bar'],
+      code:
+        '/** @customFunction */\r\nfunction foo() {\r\n    return 42;\r\n}\r\n/** @customFunction */\r\nfunction bar() {\r\n    return 43;\r\n}\r\n',
+      jsLibs: [
+        'https://unpkg.com/core-js@2.4.1/client/core.min.js',
+        'https://unpkg.com/@microsoft/office-js-helpers@0.7.4/dist/office.helpers.min.js',
+        'https://unpkg.com/jquery@3.1.1',
+      ],
+    },
+    {
+      solutionId: '2323423432',
+      namespace: 'BlankSnippet',
+      functionNames: ['add10', 'add5ish'],
+      code:
+        '/** @CustomFunction */\r\nfunction add10(x) {\r\n    return x + 10;\r\n}\r\n/** @CustomFunction */\r\nfunction add5ish(x) {\r\n    var num = Math.random();\r\n    console.log(num);\r\n    return x + num + 5;\r\n}\r\n',
+      jsLibs: [
+        'https://unpkg.com/core-js@2.4.1/client/core.min.js',
+        'https://unpkg.com/@microsoft/office-js-helpers@0.7.4/dist/office.helpers.min.js',
+        'https://unpkg.com/jquery@3.1.1',
+      ],
+    },
+  ];
+}
