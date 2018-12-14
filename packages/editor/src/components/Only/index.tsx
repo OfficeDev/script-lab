8d352356bd6ccea45b548f8b4b7708a9560799b1
Nico Bellante <nicobell@microsoft.com>
2018-12-14 11:46:27 -0800

    cleanup

--- packages/editor/src/components/Only/index.tsx
+++
@@ -1,1 +0,0 @@
-export default ({ when, children }) => (when ? children : null);
---
+++ packages/editor/src/pages/Editor/Editor.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import IDE from '../../components/IDE';
+
+import { connect } from 'react-redux';
+import actions from '../../store/actions';
+
+class Editor extends React.Component<{ initialize: () => void }> {
+  constructor(props) {
+    super(props);
+    this.props.initialize();
+  }
+
+  render() {
+    return <IDE />;
+  }
+}
+
+export default connect(
+  null,
+  { initialize: actions.misc.initialize },
+)(Editor);
