bcc6ad7e208e686a24393ab112142f281abc4143
Zlatkovsky <michael.zlat@gmail.com>
2018-12-28 13:11:15 -0800

    Reverting Monaco to 0.10.1 to avoid Mac issue where can't click anywhere except last line.
    Also introduced a "monaco-test.html" that can easily be used for testing.
    
    Simply create a snippet with:
    ```
    window.top.location.href =
        "https://script-lab-react-beta.azurewebsites.net/monaco-test.html";
    ```
    And then run it.  From there, you will be able to substitute any
    version numbers/URLs that you want.
    
    In the process of changing out the version numbers,
    refactoring the logic a bit to make future adjustments easier
    and more centralized.

---
+++ packages/common/src/package-versions.ts
@@ -0,0 +1,16 @@
+export const PACKAGE_VERSIONS = {
+  'monaco-editor': '0.10.1',
+  '@microsoft/office-js': '1.1.11-adhoc.28',
+};
+
+export const HYPHENATED_PACKAGE_VERSIONS: typeof PACKAGE_VERSIONS = (() => {
+  const result = {} as any;
+  for (const key in PACKAGE_VERSIONS) {
+    result[key] = hyphenate((PACKAGE_VERSIONS as any)[key] as any);
+  }
+  return result;
+})();
+
+export function hyphenate(versionString: string) {
+  return versionString.replace(/\./g, '-');
+}
--- packages/editor/src/components/IDE/Editor/ReactMonaco.tsx
+++ packages/editor/src/components/IDE/Editor/ReactMonaco.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const monacoEditorVersionNumber = '0-14-3';
+const monacoEditorVersionNumber = '0.10.1'.replace(/\./, '-');
 
 export interface IProps {
   solutionId: string;
