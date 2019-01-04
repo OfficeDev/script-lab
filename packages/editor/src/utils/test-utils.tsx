26e5e90321ec653d2e2fdb2b6e37103e1047b2c8
nico bellante <nicobell@microsoft.com>
2019-01-03 19:48:50 -0800

    updating existing test files

--- packages/editor/src/utils/test-utils.tsx
+++ packages/editor/src/utils/test-utils.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 
+import Theme from 'common/lib/components/Theme';
+import configureStore from '../pages/Editor/store/configureStore';
 import { Provider } from 'react-redux';
 
-import Theme from 'common/lib/components/Theme';
-
-import configureStore from '../pages/Editor/store/configureStore';
-
-const customRender = node => {
+export const renderWithTheme = node => {
   return render(<Theme host={'EXCEL'}>{node}</Theme>);
 };
 
 // re-export everything
 export * from 'react-testing-library';
 
-// override render method
-export { customRender as render };
-
-export function renderWithReduxAndRouter(ui, { initialState }) {
+export const renderWithRedux = (ui, { initialState }) => {
   const store = configureStore({ initialState });
-  return {
-    ...customRender(<Provider store={store}>{ui}</Provider>),
-    // adding `store` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    store,
-  };
-}
+  return { ...render(<Provider store={store}>{ui}</Provider>), store };
+};
