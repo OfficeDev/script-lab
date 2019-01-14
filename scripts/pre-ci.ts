a1a01ac67d1db96c6277c977a0d3542cde8f5f7d
Zlatkovsky <michael.zlat@gmail.com>
2019-01-14 14:08:51 -0800

    Fix pre-ci logic to work when there are 0 matches.

--- scripts/pre-ci.ts
+++ scripts/pre-ci.ts
@@ -4,26 +4,48 @@ import childProcess from 'child_process';
 // any code that we didn't want committed by accident.
 
 // The keywords (written here with extra spaces, so that it itself doesn't provide a match)
-const keywordsToFailOn = ['F I X M E', 'S T O P S T O P'];
+// cspell:ignore stopstop
+const keywordsToFailOn = ['FIXME', 'STOPSTOP'];
 
-keywordsToFailOn.forEach(rawWord => {
-  const match = rawWord.replace(/ /g, '');
-  const command = `git grep --ignore-case ${match}`;
-  console.log('Running command: `' + command + '`');
+try {
+  keywordsToFailOn.forEach(rawWord => {
+    const match = rawWord.replace(/ /g, '');
+    const command = `git grep --ignore-case ${match}`;
+    console.log('Running command: `' + command + '`');
 
-  const result = childProcess
-    .execSync(command)
-    .toString()
-    .trim();
-  if (result.length > 0) {
-    console.error(
-      'Found flagged words within the git directory files. Failing pre-ci check.',
-    );
-    console.error('Here were the matching entries:');
-    console.warn(result);
-    process.exit(1);
-  }
-});
+    let results = childProcess
+      .execSync(command)
+      .toString()
+      .trim()
+      .split('\n')
+      .map(line => line.trim())
+      .filter(line => line.length > 0);
 
-console.log(`=== Done running pre-ci script ===`);
-process.exit(0);
+    // Expecting to find some results (namely, in this file).
+    // If it didn't find anything, that something is amiss.
+    // Note: intentionally having it find something in at least one file (this file)
+    //     or else "git grep" returns a non-zero exit code on 0 matches!
+    if (results.length < 1) {
+      throw new Error('Unexpected');
+    }
+
+    results = results.filter(line => !line.startsWith('scripts/pre-ci.ts:'));
+
+    // After filtering out, expecting 0 matches
+    if (results.length > 0) {
+      console.error(
+        'Found flagged words within the git directory files. Failing pre-ci check.',
+      );
+      console.error('Here were the matching entries:');
+      console.warn(results.map(line => ' - ' + line).join('\n'));
+      process.exit(1);
+    }
+  });
+
+  console.log(`=== Done running pre-ci script ===`);
+  process.exit(0);
+} catch (e) {
+  console.error('Unexpected error');
+  console.error(e);
+  process.exit(1);
+}
