d80c678dcf80e88089d689314fdaaa226c9d7e10
Zlatkovsky <michael.zlat@gmail.com>
2019-01-14 17:17:49 -0800

    Fix sanitization of HTML in error display page

--- packages/runner/src/pages/Runner/components/SnippetContainer/templates/sanitizer.ts
+++ packages/runner/src/pages/Runner/components/SnippetContainer/templates/sanitizer.ts
@@ -2,7 +2,13 @@ import sanitize from 'sanitize-html';
 
 export function sanitizeObject<T>(obj: T): T {
   return Object.keys(obj).reduce(
-    (all, argName) => ({ ...all, [argName]: sanitize(obj[argName]) }),
+    (all, argName) => ({
+      ...all,
+      [argName]: sanitize(obj[argName], {
+        allowedTags: [],
+        allowedAttributes: {},
+      }),
+    }),
     {},
   ) as T;
 }
