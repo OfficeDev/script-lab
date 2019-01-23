263997e7a74be01c6fe975f172f48b3e115a0bef
Michael Zlatkovsky <Zlatkovsky@users.noreply.github.com>
2019-01-23 12:12:27 -0800

    Fix issue with Office 2016 RTM (#582)
    Fix issue with Office 2016 RTM

--- packages/common/src/utilities/error.ts
+++ packages/common/src/utilities/error.ts
@@ -4,8 +4,8 @@ import { CustomError } from '@microsoft/office-js-helpers';
  * A class for specifying an Error object with some inner details
  */
 export class ScriptLabError extends CustomError {
-  constructor(message: string, innerError?: Error) {
-    super('Script Lab Error', message, innerError);
+  constructor(message: string, innerError?: Error | string) {
+    super('Script Lab Error', message, innerError as any);
     Object.setPrototypeOf(this, ScriptLabError.prototype);
   }
 }
