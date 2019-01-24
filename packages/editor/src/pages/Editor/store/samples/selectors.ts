320629921b0b7ca01e9fb181491e6afed55d39bb
Zlatkovsky <michael.zlat@gmail.com>
2019-01-23 18:17:04 -0800

    Fix null `state.samples` crash

--- packages/editor/src/pages/Editor/store/samples/selectors.ts
+++ packages/editor/src/pages/Editor/store/samples/selectors.ts
@@ -1,7 +1,7 @@
 import { IState } from '../reducer';
 
 export const getMetadataByGroup = (state: IState): ISampleMetadataByGroup =>
-  Object.values(state.samples)
+  Object.values(state.samples || [])
     .filter(sample => sample.host === state.host)
     .reduce(
       (byGroup, sample) => ({
