f88e4e740bc659f81308fb195f26476683d23691
Zlatkovsky <michael.zlat@gmail.com>
2019-03-07 18:15:17 -0800

    Move interface

--- packages/editor/src/interfaces/custom.functions.d.ts
+++ packages/editor/src/interfaces/custom.functions.d.ts
@@ -1,8 +1,9 @@
 /* The parsed result, as processed by Script Lab.
  * Be sure to pass "IFunction" from the "custom-functions-metadata" package as the "T" argument.
+ * This is necessary for keeping the ICustomFunctionParseResult declaration ambient
+ *   (or else it would require always importing this file)
  */
 interface ICustomFunctionParseResult<T> {
-  // FIXME check if <T> is needed
   /** The as-written name of the function (no namespace/sub-namespace, not capitalized. E.g., "add42") */
   javascriptFunctionName: string;
 
