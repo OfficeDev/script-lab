50071ef492ab0cf1c1b66376a9f9e49b932c81b7
Michael Zlatkovsky <michael.zlat@gmail.com>
2019-07-30 21:25:29 -0700

    Add ability to language-switch between TS and Python (When settings are set for jupyter)

--- packages/editor/src/pages/Editor/store/footer/selectors.ts
+++ packages/editor/src/pages/Editor/store/footer/selectors.ts
@@ -9,9 +9,9 @@ import {
 
 // selectors
 import { createSelector } from 'reselect';
-import { getActiveFile } from '../editor/selectors';
+import { getActiveFile, getActiveSolution } from '../editor/selectors';
 import { getIsWeb, get as getHost } from '../host/selectors';
-import { getMode, IHeaderItem } from '../header/selectors';
+import { getMode } from '../header/selectors';
 import { getPrettyEditorTheme } from '../settings/selectors';
 
 // actions
@@ -26,6 +26,9 @@ import {
   solutions,
   settings,
 } from '../actions';
+import { getPythonConfigIfAny } from '../../../../utils/python';
+import { SCRIPT_FILE_NAME } from 'common/lib/utilities/solution';
+import languageMap from '../../../../utils/languageMap';
 
 const actions = {
   dialog,
@@ -37,15 +40,6 @@ const actions = {
   misc,
   solutions,
   settings,
-};
-
-const languageMap = {
-  typescript: 'TypeScript',
-  javascript: 'JavaScript',
-  css: 'CSS',
-  html: 'HTML',
-  json: 'JSON',
-  python: 'Python',
 };
 
 export const getItems = createSelector(
@@ -91,9 +85,10 @@ export const getItems = createSelector(
 );
 
 export const getFarItems = createSelector(
-  [getMode, getActiveFile, getPrettyEditorTheme],
+  [getMode, getActiveSolution, getActiveFile, getPrettyEditorTheme],
   (
     mode: 'normal' | 'settings' | 'null-solution',
+    activeSolution: ISolution,
     activeFile: IFile,
     currentEditorTheme: string,
   ) => [
@@ -101,6 +96,22 @@ export const getFarItems = createSelector(
       hidden: !languageMap[activeFile.language.toLowerCase()],
       key: 'editor-language',
       text: languageMap[activeFile.language.toLowerCase()],
+      subMenuProps:
+        activeFile.name === SCRIPT_FILE_NAME && getPythonConfigIfAny()
+          ? {
+              isBeakVisible: true,
+              items: [languageMap.typescript, languageMap.python].map(language => ({
+                key: language,
+                text: language,
+                actionCreator: () =>
+                  actions.solutions.changeLanguage({
+                    solutionId: activeSolution.id,
+                    fileId: activeFile.id,
+                    language: language,
+                  }),
+              })),
+            }
+          : null,
     },
     {
       hidden: mode === 'settings',
