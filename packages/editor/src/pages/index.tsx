eb46018440b22c0522fce8f20ddca1f66038eb5e
Michael Zlatkovsky <Zlatkovsky@users.noreply.github.com>
2020-03-24 22:15:22 -0700

    Fix help/referenceDocs/etc ribbon buttons not opening (#791)

--- packages/editor/src/pages/index.tsx
+++ packages/editor/src/pages/index.tsx
@@ -24,7 +24,7 @@ const pages: { [key: string]: IPageLoadingSpec } = {
   [PATHS.AddinCommands]: {
     component: AddinCommands,
     officeJs: SCRIPT_URLS.DEFAULT_OFFICE_JS,
-    skipOfficeOnReady: true /* skip calling "Office.onReady" until the redirect is determined.
+    skipOfficeOnReady: true /* skip calling "Office.onReady" until the functions are registered.
       The component's setup will then call it itself */,
     skipRedirect: true /* skip redirecting. It won't work for add-in commands anyway,
       and the logic is so simple we can just always rely on the prod version */,
@@ -48,7 +48,8 @@ const pages: { [key: string]: IPageLoadingSpec } = {
   },
   [PATHS.External]: {
     component: External,
-    officeJs: null /* does a window.location redirect, doesn't need Office.js */,
+    officeJs: SCRIPT_URLS.DEFAULT_OFFICE_JS, // Need Office.js so that
+    //   can message back to parent taskpane to close the dialog
   },
   [PATHS.Heartbeat]: {
     component: Heartbeat,
