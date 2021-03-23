7cccb4589bbb04e4382439bd7544041194c7a34c
raulclouse <raulclouse@hotmail.com>
2021-03-23 16:47:54 -0700

    fix unit test

--- packages/editor/src/pages/CustomFunctions/components/Summary/__tests__/Summary.test.tsx
+++ packages/editor/src/pages/CustomFunctions/components/Summary/__tests__/Summary.test.tsx
@@ -2,7 +2,72 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import { Summary } from '../';
-import { basicSummaryProps, BasicSummary } from '../Summary.stories';
+
+enum Status {
+  Good = 'good',
+  Skipped = 'skipped',
+  Error = 'error',
+  Untrusted = 'untrusted',
+}
+
+const funcName = 'foo';
+const nonCapitalizedFullName = 'SnippetName';
+const basicSummaryProps: {
+  items: Array<ICustomFunctionParseResult<any>>;
+} = {
+  items: [
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}1`,
+      status: Status.Error,
+      metadata: null,
+    },
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}2`,
+      status: Status.Skipped,
+      metadata: null,
+    },
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}3`,
+      status: Status.Error,
+      metadata: null,
+    },
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}4`,
+      status: Status.Skipped,
+      metadata: null,
+    },
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}5`,
+      status: Status.Good,
+      metadata: null,
+    },
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}6`,
+      status: Status.Good,
+      metadata: null,
+    },
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}7`,
+      status: Status.Good,
+      metadata: null,
+    },
+    {
+      nonCapitalizedFullName,
+      javascriptFunctionName: `${funcName}8`,
+      status: Status.Good,
+      metadata: null,
+    },
+  ],
+};
+
+const BasicSummary = () => <Summary {...basicSummaryProps} />;
 
 describe('Summary should render properly in basic case', () => {
   it('should not crash', () => {
