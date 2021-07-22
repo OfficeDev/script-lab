eed8e0e8a042a010e89192772be3e9e04d0c5797
Michael Vaquier <mvaquier@microsoft.com>
2021-07-22 13:03:45 -0700

    Added install steps to pipeline

--- scripts/azure.pipelines/pipeline.install.ps1
+++ scripts/azure.pipelines/pipeline.install.ps1
@@ -1,4 +1,10 @@
-yarn install --frozen-lockfile
-yarn pre-ci || exit 1
-yarn postinstall
-git --no-pager diff HEAD --exit-code || exit 1 # This makes sure that someone ran "yarn install" after merging their PR, and that the postinstall doesn't produce any diffs, which are annoying/confusing
+$diffs=$(git --no-pager diff HEAD --exit-code)
+
+if($diff -ne 0){
+    Write-Host "Diffs were found"
+    exit 1
+}
+else {
+    Write-Host "No Diffs Found"
+    exit 0
+}
