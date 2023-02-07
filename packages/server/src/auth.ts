dec366249b4b2db162ec8f261eaf69823e0feeef
Raul Clouse <rclouse@microsoft.com>
2023-02-06 17:00:28 -0800

    use xmlhttprequest

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_REDIRECT_URL } = process.env;
 const GENERIC_ERROR_STRING = 'An unexpected login error has occurred.';
 
@@ -12,7 +10,7 @@ export interface IGithubApiAccessTokenResponse {
 export function getAccessTokenOrErrorResponse(
   input: IServerAuthRequest,
 ): Promise<IServerAuthResponse> {
-  return new Promise(resolve => {
+  let postRequest = new Promise((resolve, reject) => {
     const url = 'https://github.com/login/oauth/access_token';
     const data = {
       client_id: GITHUB_CLIENT_ID,
@@ -21,28 +19,44 @@ export function getAccessTokenOrErrorResponse(
       code: input.code,
       state: input.state,
     };
-    const config = { headers: { Accept: 'application/json' } };
 
-    axios
-      .post(url, data, config)
-      .then(response => {
-        resolve(getResultObjectBasedOnAuthResponse(response.data));
-      })
-      .catch(error => {
-        resolve({ error: error });
-      });
+    let request = new XMLHttpRequest();
 
-    // Helper
-    function getResultObjectBasedOnAuthResponse(
-      body: IGithubApiAccessTokenResponse,
-    ): IServerAuthResponse {
-      if (body.error) {
-        return { error: body.error + ': ' + body.error_description };
-      } else if (body.access_token) {
-        return { access_token: body.access_token };
-      } else {
-        return { error: GENERIC_ERROR_STRING };
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        let responseData: IGithubApiAccessTokenResponse = JSON.parse(
+          request.responseText,
+        );
+        resolve(responseData);
+      } else if (request.readyState === 4) {
+        reject('Failure retrieving data');
       }
-    }
+    });
+
+    request.open('POST', url);
+    request.setRequestHeader('Accept', 'application/json');
+    request.send(data);
   });
+  return postRequest
+    .then(response => {
+      return getResultObjectBasedOnAuthResponse(
+        response as IGithubApiAccessTokenResponse,
+      );
+    })
+    .catch(() => {
+      return { error: GENERIC_ERROR_STRING };
+    });
+
+  // Helper
+  function getResultObjectBasedOnAuthResponse(
+    body: IGithubApiAccessTokenResponse,
+  ): IServerAuthResponse {
+    if (body.error) {
+      return { error: body.error + ': ' + body.error_description };
+    } else if (body.access_token) {
+      return { access_token: body.access_token };
+    } else {
+      return { error: GENERIC_ERROR_STRING };
+    }
+  }
 }
