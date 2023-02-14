fbbf89faeede240c0b4cfa633b09a91b5fb835e9
wandyezj <wandyezj@gmail.com>
2023-02-13 22:12:03 -0800

    Move server auth to use built in node https (#984)

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -1,62 +1,104 @@
 const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_REDIRECT_URL } = process.env;
 const GENERIC_ERROR_STRING = 'An unexpected login error has occurred.';
 
-export interface IGithubApiAccessTokenResponse {
+export interface GithubApiAccessTokenResponse {
   access_token: string;
   error: string;
   error_description: string;
 }
 
-export function getAccessTokenOrErrorResponse(
+const https = require('https');
+
+async function github_auth_post(code: string, state: string) {
+  // https://github.com/login/oauth/access_token
+
+  const options = {
+    host: 'github.com',
+    method: 'POST',
+    path: '/login/oauth/access_token',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const postData = {
+    client_id: GITHUB_CLIENT_ID,
+    client_secret: GITHUB_CLIENT_SECRET,
+    redirect_uri: GITHUB_REDIRECT_URL,
+    code,
+    state,
+  };
+  const data = JSON.stringify(postData);
+
+  // Good to know what is actually being sent
+  // console.log(options)
+  // console.log(data)
+
+  return new Promise<{ status: number; buffer: () => Promise<string> }>(
+    (resolve, reject) => {
+      const request = https.request(options, (response: any) => {
+        const statusCode = response.statusCode;
+        response.setEncoding('utf8');
+
+        response.on('error', (err: any) => {
+          reject(err);
+        });
+
+        // collect the data
+        let data = '';
+
+        response.on('data', (chunk: string) => {
+          data += chunk;
+        });
+
+        const dataBuffer = new Promise<string>(resolveBuffer => {
+          response.on('end', () => {
+            resolveBuffer(data);
+          });
+        });
+
+        resolve({
+          status: statusCode || -1,
+          buffer: () => dataBuffer,
+        });
+      });
+
+      request.on('error', (e: any) => {
+        //console.log("Error : " + e.message);
+        reject(e);
+      });
+
+      request.write(data);
+      request.end();
+    },
+  );
+}
+
+function getResultObjectBasedOnAuthResponse(
+  body: GithubApiAccessTokenResponse,
+): IServerAuthResponse {
+  if (body.error) {
+    return { error: body.error + ': ' + body.error_description };
+  } else if (body.access_token) {
+    return { access_token: body.access_token };
+  } else {
+    return { error: GENERIC_ERROR_STRING };
+  }
+}
+
+export async function getAccessTokenOrErrorResponse(
   input: IServerAuthRequest,
 ): Promise<IServerAuthResponse> {
-  let postRequest = new Promise((resolve, reject) => {
-    const url = 'https://github.com/login/oauth/access_token';
-    const data = {
-      client_id: GITHUB_CLIENT_ID,
-      client_secret: GITHUB_CLIENT_SECRET,
-      redirect_uri: GITHUB_REDIRECT_URL,
-      code: input.code,
-      state: input.state,
-    };
-
-    let request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState === 4 && request.status === 200) {
-        let responseData: IGithubApiAccessTokenResponse = JSON.parse(
-          request.responseText,
-        );
-        resolve(responseData);
-      } else if (request.readyState === 4) {
-        reject('Failure retrieving data');
-      }
-    });
-
-    request.open('POST', url);
-    request.setRequestHeader('Accept', 'application/json');
-    request.send(data);
-  });
-  return postRequest
-    .then(response => {
-      return getResultObjectBasedOnAuthResponse(
-        response as IGithubApiAccessTokenResponse,
-      );
-    })
-    .catch(() => {
-      return { error: GENERIC_ERROR_STRING };
-    });
-
-  // Helper
-  function getResultObjectBasedOnAuthResponse(
-    body: IGithubApiAccessTokenResponse,
-  ): IServerAuthResponse {
-    if (body.error) {
-      return { error: body.error + ': ' + body.error_description };
-    } else if (body.access_token) {
-      return { access_token: body.access_token };
-    } else {
-      return { error: GENERIC_ERROR_STRING };
-    }
+  const { status, buffer } = await github_auth_post(input.code, input.state);
+  // console.log(status)
+  if (status !== 200) {
+    return { error: GENERIC_ERROR_STRING };
   }
+
+  const response = await buffer();
+  // console.log(response);
+  const data = JSON.parse(response) as GithubApiAccessTokenResponse;
+  const result = getResultObjectBasedOnAuthResponse(data);
+  return result;
 }
