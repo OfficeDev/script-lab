e64bb5de4831d168edc2fed88967a27d2038adec
jowandy <wandyezj@gmail.com>
2023-11-02 14:21:02 -0700

    Update server cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // config
+// https://www.npmjs.com/package/cors/v/2.8.5
 app.use(cors());
 app.use(express.json() as any);
 
@@ -25,6 +26,9 @@ app.get('/' + SERVER_HELLO_ENDPOINT.path, (_req, res) => {
     .status(200)
     .send(SERVER_HELLO_ENDPOINT.payload);
 });
+
+// enable options at the auth endpoint.
+app.options('/auth', cors());
 
 // An auth endpoint for GitHub that returns a JSON payload of type IServerAuthResponse
 app.post('/auth', async (req, res) => {
