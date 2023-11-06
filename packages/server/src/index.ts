d0ee493edeffbbcd2a0d5da4b54de36cc15950d0
Joe Wandyez <jowandy@microsoft.com>
2023-11-06 11:16:03 -0800

    minor change

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // config
+
+// express.json middleware allows handling request JSON payloads
 app.use(express.json() as any);
 
 // routes
@@ -24,10 +26,11 @@ app.get('/hello', (_req, res) => {
 
 app.options('/auth', async (_req, res) => {
   res
-    .header('Access-Control-Allow-Headers', 'content-type')
-    .header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-    .header('Access-Control-Allow-Origin', '*')
-    .header('Content-Length', '0')
+    .set({
+      'Access-Control-Allow-Headers': 'content-type',
+      'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      'Access-Control-Allow-Origin': '*',
+    })
     .status(204);
 });
 
@@ -48,16 +51,17 @@ app.post('/auth', async (req, res) => {
 
   res
     .contentType('application/json')
-    .header('Access-Control-Allow-Origin', '*')
+    .set({ 'Access-Control-Allow-Origin': '*' })
     .status(200)
     .send(responsePayload);
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
 app.get('/test', (_req, res) => {
   res
     .contentType('text/plain')
+    .header({ 'Access-Control-Allow-Origin': '*' })
     .status(200)
     .send('test response 1');
 });
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
